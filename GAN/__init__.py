af5e8d346ab70853fb4e8683a08af5f0700be95d
xiaoran <xiaoran2495897824@163.com>
2024-06-05 19:25:52 +0800

    update: init

--- GAN/__init__.py
+++ GAN/__init__.py
@@ -1,5 +1,7 @@
 from .backend import get_default_device
 from .util import get_generator
+from .model import Generator
+from .model import Discriminator
 
 
-__all__ = ["get_default_device", "get_generator"]
+__all__ = ["get_default_device", "get_generator", "Generator", "Discriminator"]

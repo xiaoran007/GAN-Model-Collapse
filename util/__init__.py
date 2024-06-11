3c44d2b3ed806f41d3be236318ffabda2d9b210e
xiaoran <xiaoran2495897824@163.com>
2024-06-11 12:10:31 +0800

    update: util package

---
+++ util/__init__.py
@@ -0,0 +1,6 @@
+from .util import split_dataset_random
+from .util import GANs_two_class_real_data
+from .util import get_minor_major
+
+
+__all__ = ["split_dataset_random", "GANs_two_class_real_data", "get_minor_major"]
---
+++ util/util.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+def split_dataset_random(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None, stratify=y)
+
+    return X_train, y_train, X_test, y_test
+
+
+def GANs_two_class_real_data(X_train, y_train):
+    X_real = []
+    y_train = y_train.ravel()
+    for i in range(len(y_train)):
+        if int(y_train[i]) == 1:
+            X_real.append(X_train[i])
+    X_real = np.array(X_real)
+    y_real = np.ones((X_real.shape[0],))
+    return X_real, y_real
+
+
+def get_minor_major(y):
+    minor, major = 0, 0
+    for i in y:
+        if i == 1:
+            minor += 1
+        else:
+            major += 1
+    return minor, major
+
+

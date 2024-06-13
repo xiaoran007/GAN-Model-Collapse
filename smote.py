5a8d97d03cba3acd904ff8085b0ee2fd7375b0eb
xiaoran <xiaoran2495897824@163.com>
2024-06-13 21:29:49 +0800

    update: fix tsne for real and generated data

--- smote.py
+++ smote.py
@@ -52,20 +52,41 @@ def GANs_two_class_real_data(X_train, y_train):  # Defining the real data for GA
 
 X_real, y_real = GANs_two_class_real_data(X_train, y_train)
 X_train_SMOTE_gen = X_train_SMOTE[X_train.shape[0]:]
-X_train_SMOTE_sel = resample(X_train_SMOTE_gen, n_samples=X_real.shape[0])
+# X_train_SMOTE_sel = resample(X_train_SMOTE_gen, n_samples=X_real.shape[0])
+X_train_SMOTE_sel = X_train_SMOTE_gen[np.random.choice(X_train_SMOTE_gen.shape[0], size=X_real.shape[0], replace=False)]
 
-tsne = TSNE(n_components=2, random_state=42, verbose=1, angle=0.2, perplexity=100)
-X_embedded = tsne.fit_transform(X_real)
+print(type(X_train_SMOTE_sel))
 
-x_gen = TSNE(n_components=2, random_state=42, verbose=1, angle=0.2, perplexity=100).fit_transform(X_train_SMOTE_sel)
+total_x = np.concatenate((X_real, X_train_SMOTE_sel), axis=0)
 
-print(trustworthiness(X_real, X_embedded, n_neighbors=5))
-print(trustworthiness(X_train_SMOTE_sel, x_gen, n_neighbors=5))
+tsne = TSNE(n_components=2, random_state=42, verbose=1, angle=0.2, perplexity=50)
+X_embedded = tsne.fit_transform(total_x)
+# X_embedded = tsne.fit_transform(X_real)
+#
+# x_gen = TSNE(n_components=2, random_state=42, verbose=1, angle=0.2, perplexity=50).fit_transform(X_train_SMOTE_sel)
+#
+print(trustworthiness(total_x, X_embedded, n_neighbors=5))
+# print(trustworthiness(X_train_SMOTE_sel, x_gen, n_neighbors=5))
+#
 
+num_real_samples = X_real.shape[0]
+
+# Plotting
 plt.figure(figsize=(10, 8))
-plt.scatter(X_embedded[:, 0], X_embedded[:, 1], label='Original Data', alpha=0.6, c='blue')
-plt.scatter(x_gen[:, 0], x_gen[:, 1], label='Generated Data', alpha=0.6, c='red')
+
+# Plot real data
+plt.scatter(X_embedded[:num_real_samples, 0], X_embedded[:num_real_samples, 1], label='Original Data', alpha=0.6, c='blue')
+
+# Plot generated data
+plt.scatter(X_embedded[num_real_samples:, 0], X_embedded[num_real_samples:, 1], label='Generated Data', alpha=0.6, c='red')
+
+# Add legend
 plt.legend()
+
+# plt.figure(figsize=(10, 8))
+# plt.scatter(X_embedded[:, 0], X_embedded[:, 1], label='Original Data', alpha=0.6, c='blue')
+# plt.scatter(x_gen[:, 0], x_gen[:, 1], label='Generated Data', alpha=0.6, c='red')
+# plt.legend()
 plt.title("t-SNE visualization of Original and Generated Data")
 plt.xlabel("t-SNE feature 1")
 plt.ylabel("t-SNE feature 2")

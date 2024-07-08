207faf81ea4a58aceed1ea984ff6dfc7e19c8697
xiaoran <xiaoran2495897824@163.com>
2024-07-08 21:35:12 +0800

    update: init.py

--- plots/__init__.py
+++ plots/__init__.py
@@ -4,6 +4,7 @@ from .Tsne import CTGANTsne
 from .Tsne import Imgs
 from .Tsne import TsneInit
 from .AllTsne import AllTsne
+from .AllTsneWithMajority import AllTsne as AllTsneWithMajority
 
 
-__all__ = ["GANTsne", "SMOTETsne", "CTGANTsne", "Imgs", "TsneInit", "AllTsne"]
+__all__ = ["GANTsne", "SMOTETsne", "CTGANTsne", "Imgs", "TsneInit", "AllTsne", "AllTsneWithMajority"]
